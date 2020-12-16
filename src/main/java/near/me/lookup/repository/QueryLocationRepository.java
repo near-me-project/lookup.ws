382cd18978a3b3c5152a9847ceb15d3f16831e93
tsekot sergey <tsekot.sergey@gmail.com>
2020-12-16 16:40:13 +0100

    adding query support

--- src/main/java/near/me/lookup/repository/QueryLocationRepository.java
+++ src/main/java/near/me/lookup/repository/QueryLocationRepository.java
@@ -1,8 +1,8 @@
 package near.me.lookup.repository;
 
 import near.me.lookup.controller.model.request.FilterCriteria;
-import near.me.lookup.repository.querybuilders.CriteriaBuilder;
 import near.me.lookup.repository.entity.Location;
+import near.me.lookup.repository.querybuilders.CriteriaBuilder;
 import near.me.lookup.service.LocationService;
 import near.me.lookup.service.domain.LocationDto;
 import near.me.lookup.shared.ModelMapper;
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -41,16 +40,9 @@ public class QueryLocationRepository {
                 .and("description", contains(filter.getDescription()))
                 .$();
 
+        List<Location> locations = mongoTemplate.find(Query.query(criteria), Location.class);
+        return locations.stream().map(l -> ModelMapper.map(l, LocationDto.class)).collect(Collectors.toList());
 
-        if (false) {
-            TextCriteria textCriteria = TextCriteria.forDefaultLanguage().matching(filter.getDescription());
-            List<Location> locations = mongoTemplate.find(Query.query(criteria).addCriteria(textCriteria), Location.class);
-            return locations.stream().map(l -> ModelMapper.map(l, LocationDto.class)).collect(Collectors.toList());
-
-        } else {
-            List<Location> locations = mongoTemplate.find(Query.query(criteria), Location.class);
-            return locations.stream().map(l -> ModelMapper.map(l, LocationDto.class)).collect(Collectors.toList());
-        }
     }
 }
 
